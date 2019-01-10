664ce7ce5aa73ec1bf7edbb71cb5ecbb998be30d
Pavel Rodionoff <rodionoff.spb@gmail.com>
2019-01-10 08:12:04 +0300

    lazyload promise way

--- src/js/lazyLoad.js
+++ src/js/lazyLoad.js
@@ -2,7 +2,9 @@ let images = document.getElementsByClassName('photo-image')
 
 document.addEventListener("DOMContentLoaded", () => {
   Array.prototype.forEach.call(images, (image, i) => {
-    image.lastElementChild.src = `./src/img/krooshkin/${i+1}.jpeg`
-    console.log(image.lastElementChild.src)
+    // image.lastElementChild.src = `./src/img/krooshkin/${i+1}.jpeg`
+    new Promise(resolve => {
+      resolve(image.lastElementChild.src = `./src/img/krooshkin/${i+1}.jpeg`)
+    })
   })
 })
