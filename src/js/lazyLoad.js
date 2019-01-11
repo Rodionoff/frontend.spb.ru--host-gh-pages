d13d55dbeae0a2e80c2862a304f90ae470854645
Pavel Rodionoff <rodionoff.spb@gmail.com>
2019-01-11 14:00:15 +0300

    shit

--- src/js/lazyLoad.js
+++ src/js/lazyLoad.js
@@ -1,15 +1,19 @@
 let images = document.getElementsByClassName('photo-image')
 
 document.addEventListener("DOMContentLoaded", () => {
-  Array.prototype.forEach.call(images, function(ChildIsImage, i) {
-
+  for (let i = images.length - 1; i >= 0; i -= 1) {
+    // обратный порядок здесь для того чтобы
+    // onload попадал в стек в обратном порядке
+    // то есть первыми в стек попадают последние
+    // изображения а значит они же последними и выходят
     let downloadedImg = new Image()
+
+    // console.log(i)
     
-    // console.log(ChildIsImage)
     downloadedImg.onload = function() {
-      images[i].replaceChild(downloadedImg, ChildIsImage.children[0])
+      images[i].replaceChild(downloadedImg, images[i].children[0])
     }
 
     downloadedImg.src = `./src/img/krooshkin/${i+1}.jpeg`
-  })
+  }
 })
