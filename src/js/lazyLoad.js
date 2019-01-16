f62c1be3d1b8b7fda23f4b9bff187a90e61fbf58
Pavel Rodionoff <rodionoff.spb@gmail.com>
2019-01-16 09:24:36 +0300

    change lazyLoad

--- src/js/lazyLoad.js
+++ src/js/lazyLoad.js
@@ -5,6 +5,6 @@ document.addEventListener("DOMContentLoaded", () => {
     // обратный порядок здесь для того чтобы
     // onload попадал в стек в обратном порядке
 
-    images[i].children[0].src = `./src/img/krooshkin/${i+1}.jpeg`
+    image.children[0].src = `./src/img/krooshkin/${i+1}.jpeg`
   })
 })
