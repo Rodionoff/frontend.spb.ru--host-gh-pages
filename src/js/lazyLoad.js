f338496f27027fa1ff875409ce9d2acaacaaec51
Pavel Rodionoff <rodionoff.spb@gmail.com>
2019-01-25 10:56:04 +0300

    change lasyload

--- src/js/lazyLoad.js
+++ src/js/lazyLoad.js
@@ -1,10 +1,10 @@
 let images = document.getElementsByClassName('photo-image')
 
 document.addEventListener("DOMContentLoaded", () => {
-  Array.prototype.forEach.call(images, (image, i) => {
-    // обратный порядок здесь для того чтобы
-    // onload попадал в стек в обратном порядке
-
-    image.children[0].src = `./src/img/krooshkin/${i+1}.jpeg`
+  let promises = Array.prototype.map.call(images, (image, i) => {
+    return new Promise(resolve => {
+    resolve(image.children[0].src = `./src/img/krooshkin/${i+1}.jpeg`)
+    })
   })
+  Promise.all(promises)
 })
