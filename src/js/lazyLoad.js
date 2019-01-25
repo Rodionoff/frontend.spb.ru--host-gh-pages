2e896bc4e35419155efa8af7f37aad6bb3f0fe8f
Pavel Rodionoff <rodionoff.spb@gmail.com>
2019-01-25 11:50:26 +0300

    change lazyload

--- src/js/lazyLoad.js
+++ src/js/lazyLoad.js
@@ -1,10 +1,9 @@
 let images = document.getElementsByClassName('photo-image')
 
 document.addEventListener("DOMContentLoaded", () => {
-  let promises = Array.prototype.map.call(images, (image, i) => {
-    return new Promise(resolve => {
+  Array.prototype.map.call(images, (image, i) => {
+    new Promise(resolve => {
     resolve(image.children[0].src = `./src/img/krooshkin/${i+1}.jpeg`)
     })
   })
-  Promise.all(promises)
 })
