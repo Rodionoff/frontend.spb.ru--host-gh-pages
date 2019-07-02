26c94989abad4e6338bfddbd9cea419b621263a4
Pavel Rodionoff <rodionoff.spb@gmail.com>
2019-07-02 21:21:40 +0300

    style improvements

--- src/js/toggle.js
+++ src/js/toggle.js
@@ -1,31 +1,30 @@
-const navbar = document.getElementsByClassName('navbar')[0];
-const navbarItems = document.getElementsByClassName('navbar-items')[0];
-const navbarToggle = document.getElementsByClassName('navbar-toggle')[0];
-const html = document.getElementsByTagName('html')[0];
-const body = document.getElementsByTagName('body')[0];
-const main = document.getElementsByClassName('main')[0];
-const lines = Array.from(document.getElementsByClassName('line'));
+(() => {
+  const navbarItems = document.querySelector('.navbar-items');
+  const navbarToggle = document.querySelector('.navbar-toggle');
+  const main = document.querySelector('.main');
+  const lines = Array.from(document.getElementsByClassName('line'));
 
-navbarToggle.addEventListener('click', toggleNavbar);
+  navbarToggle.addEventListener('click', toggleNavbar);
 
-function toggleNavbar() {
-  navbarItems.classList.toggle('show');
+  function toggleNavbar() {
+    navbarItems.classList.toggle('show');
 
-  if (navbarItems.classList.contains('show')) {
-    // navbarToggle.innerHTML = 'x';
-    lines.forEach(line => line.classList.add('hiddenToggle'));
-    main.addEventListener('click', foo);
-  } else {
-    // navbarToggle.innerHTML = 'v';
-    lines.forEach(line => line.classList.remove('hiddenToggle'));
+    if (navbarItems.classList.contains('show')) {
+      // navbarToggle.innerHTML = 'x';
+      lines.forEach(line => line.classList.add('hiddenToggle'));
+      main.addEventListener('click', baz);
+    } else {
+      // navbarToggle.innerHTML = 'v';
+      lines.forEach(line => line.classList.remove('hiddenToggle'));
+    }
   }
-}
 
-function foo() {
-  navbarItems.classList.remove('show');
-  main.removeEventListener('click', foo);
-  // navbarToggle.innerHTML = 'v';
-  lines.forEach(line => line.classList.remove('hiddenToggle'))
-  console.log('works');
-}
+  function baz() {
+    navbarItems.classList.remove('show');
+    main.removeEventListener('click', baz);
+    // navbarToggle.innerHTML = 'v';
+    lines.forEach(line => line.classList.remove('hiddenToggle'))
+    // console.log('works');
+  }
 
+})()
