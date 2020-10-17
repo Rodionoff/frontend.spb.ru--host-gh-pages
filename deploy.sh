bf39e186ac7fdf7e889418e96b43412c89992fe7
Rodionoff <rodionov134@gmail.com>
2020-10-17 19:20:28 +0300

    Fix misspelling

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-npm run build && cp CNAME dist -f && cd dist && git commit -am 'deploy' && git push origin master:gh-pages
+npm run build && cp CNAME dist -f && cd dist && git commit -am 'deploy' && git push origin gh-pages
 
 cd - || return
