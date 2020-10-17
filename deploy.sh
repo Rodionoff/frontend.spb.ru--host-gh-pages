27aa697cc61a703b33f2b4617cd61c26e338e0d8
Rodionoff <rodionov134@gmail.com>
2020-10-17 17:12:13 +0300

    Change deploy.sh script (need to be replaced with github actions)

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-rm -rf dist && npm run build && cd dist && git commit -am 'deploy' && git push origin master:gh-pages
+npm run build && cd dist && git commit -am 'deploy' && git push origin master:gh-pages
 
 cd - || return
