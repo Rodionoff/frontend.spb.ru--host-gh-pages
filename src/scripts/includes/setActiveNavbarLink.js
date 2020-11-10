187906ef3c6e18353614fc5f522389dac72ded09
rodionov80 <rodionov134@gmail.com>
2020-11-10 05:50:53 +0300

    Simplify set-active-navlink script

--- src/scripts/includes/setActiveNavbarLink.js
+++ src/scripts/includes/setActiveNavbarLink.js
@@ -1,7 +1,8 @@
 (() => {
   const navlinks = document.querySelectorAll('.navlink a')
-  navlinks.forEach(navlink => navlink.classList.remove('navlink-active'))
   navlinks.forEach(navlink => {
+    navlink.classList.remove('navlink-active')
+
     const href = navlink.getAttribute('href')
     if (location.pathname.startsWith(href)) {
       navlink.classList.add('navlink-active')
