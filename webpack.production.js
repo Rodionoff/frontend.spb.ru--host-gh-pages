994755311b5166ddd305af6040c53587cf579fda
Pavel Rodionov <rodionov134@gmail.com>
2020-11-11 05:52:00 +0300

    Update webpack.production.js

--- webpack.production.js
+++ webpack.production.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const zlib = require('zlib')
 
 module.exports = merge(common, {
   mode: 'production',
@@ -12,6 +13,19 @@ module.exports = merge(common, {
           to: './'
         }
       ]
-    })
+    }),
+    new​ ​CompressionPlugin​(​{​
+      ​filename​: ​'[path][base].br'​,​
+      ​algorithm​: ​'brotliCompress'​,​
+      ​test​: ​/​\.​(​js​|​css​|​html​|​svg​)​$​/​,​
+      ​compressionOptions​: ​{​
+        ​params​: ​{​
+          ​[​zlib​.​constants​.​BROTLI_PARAM_QUALITY​]​: ​11​,​
+        ​}​,​
+      ​}​,​
+      ​threshold​: ​10240​,​
+      ​minRatio​: ​0.8​,​
+      ​deleteOriginalAssets​: ​false​,​
+    ​}​)​,​
   ]
 })
