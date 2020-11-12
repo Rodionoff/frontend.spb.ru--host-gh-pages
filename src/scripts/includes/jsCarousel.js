461096eb9dae87140c510f8ddcc1023b9a48eb99
rodionov80 <rodionov134@gmail.com>
2020-11-13 01:27:36 +0300

    Extract jsCarousel into es6 class

--- src/scripts/includes/jsCarousel.js
+++ src/scripts/includes/jsCarousel.js
@@ -1,16 +1,34 @@
-const modal = document.querySelector('.modal')
-const images = document.querySelectorAll('.photo-image')
-const imagesWrapper = document.querySelector('.photos')
+class Carousel {
+  constructor () {
+    this.modal = document.querySelector('.modal')
+    this.images = document.querySelectorAll('.photo-image')
+    this.imagesWrapper = document.querySelector('.photos')
+    this.exit = document.createElement('div')
+    this.exit.classList.add('exit')
+    this.exit.innerHTML = '⌧'
+    this.leftArrow = this.createArrow('div', 'leftArrow', '⌦')
+    this.rightArrow = this.createArrow('div', 'rightArrow', '⌦')
 
-const jsCarousel = () => {
-  const exit = document.createElement('div')
-  exit.classList.add('exit')
-  exit.innerHTML = '⌧'
+    this.imagesWrapper.addEventListener('click', (event) => {
+      const findIndex = (node) => {
+        if (node !== undefined) {
+          return Array.prototype.indexOf.call(this.images, node)
+        }
+      }
 
-  const leftArrow = createArrow('div', 'leftArrow', '⌦')
-  const rightArrow = createArrow('div', 'rightArrow', '⌦')
+      const target = event.target
 
-  function createArrow (elementName, className, sign) {
+      if (target.tagName === 'IMG') {
+        const parent = target.parentNode
+        const index = findIndex(parent)
+        this.showModal(index)
+      }
+    })
+
+    this.bindEventsToControls()
+  }
+
+  createArrow (elementName, className, sign) {
     let arrow = document.createElement(elementName)
     arrow.classList.add('arrow')
     arrow.classList.add(className)
@@ -18,72 +36,58 @@ const jsCarousel = () => {
     return arrow
   }
 
-  imagesWrapper.addEventListener('click', (event) => {
-    const findIndex = (node) => {
-      if (node !== undefined) {
-        return Array.prototype.indexOf.call(images, node)
-      }
-    }
-
-    const target = event.target
-
-    if (target.tagName === 'IMG') {
-      const parent = target.parentNode
-      const index = findIndex(parent)
-      showModal(index)
-    }
-  })
-
-  function showModal (i) {
+  showModal (i) {
     let img = document.createElement('img')
     img.classList.add('modalImage')
 
-    leftArrow.style.visibility = 'visible'
-    rightArrow.style.visibility = 'visible'
+    this.leftArrow.style.visibility = 'visible'
+    this.rightArrow.style.visibility = 'visible'
     img.pk = i
-    img.src = images[i].lastElementChild.src
-    appendElementsToModal(img)
-    modal.classList.add('showModal')
+    img.src = this.images[i].lastElementChild.src
+    this.appendElementsToModal(img)
+    this.modal.classList.add('showModal')
   }
 
-  function appendElementsToModal (img) {
-    modal.appendChild(exit)
-    modal.appendChild(leftArrow)
-    modal.appendChild(img)
-    modal.appendChild(rightArrow)
+  appendElementsToModal(img) {
+    this.modal.appendChild(this.exit)
+    this.modal.appendChild(this.leftArrow)
+    this.modal.appendChild(img)
+    this.modal.appendChild(this.rightArrow)
   }
 
-  rightArrow.onclick = function () {
-    leftArrow.style.visibility = 'visible'
-    let image = document.getElementsByClassName('modalImage')[0]
-    image.pk += 1
-    let isLastImage = images[image.pk] === undefined
+  bindEventsToControls() {
+    this.rightArrow.onclick = () => {
+      this.leftArrow.style.visibility = 'visible'
+      let image = document.getElementsByClassName('modalImage')[0]
+      image.pk += 1
+      let isLastImage = this.images[image.pk] === undefined
 
-    if (isLastImage) {
-      image.pk = 0
+      if (isLastImage) {
+        image.pk = 0
+      }
+      image.src = this.images[image.pk].lastElementChild.src
     }
-    image.src = images[image.pk].lastElementChild.src
-  }
 
-  leftArrow.onclick = function () {
-    rightArrow.style.visibility = 'visible'
-    let image = document.getElementsByClassName('modalImage')[0]
-    image.pk -= 1
-    let isFirstImage = images[image.pk] === undefined
+    this.leftArrow.onclick = () => {
+      this.rightArrow.style.visibility = 'visible'
+      let image = document.getElementsByClassName('modalImage')[0]
+      image.pk -= 1
+      let isFirstImage = this.images[image.pk] === undefined
 
-    if (isFirstImage) {
-      image.pk = images.length - 1
+      if (isFirstImage) {
+        image.pk = this.images.length - 1
+      }
+      image.src = this.images[image.pk].lastElementChild.src
     }
-    image.src = images[image.pk].lastElementChild.src
-  }
 
-  exit.onclick = function () {
-    let image = document.getElementsByClassName('modalImage')[0]
-    modal.removeChild(image)
-    leftArrow.style.visibility = 'hidden'
-    rightArrow.style.visibility = 'hidden'
-    modal.classList.remove('showModal')
+    this.exit.onclick = () => {
+      let image = document.getElementsByClassName('modalImage')[0]
+      this.modal.removeChild(image)
+      this.leftArrow.style.visibility = 'hidden'
+      this.rightArrow.style.visibility = 'hidden'
+      this.modal.classList.remove('showModal')
+    }
   }
 }
 
-modal && images && imagesWrapper && jsCarousel()
+new Carousel()
