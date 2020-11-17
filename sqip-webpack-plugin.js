7cab9b00a3e80daa6567c50f9a154966399b1a28
rodionov80 <rodionov134@gmail.com>
2020-11-17 03:31:40 +0300

    Lint

--- sqip-webpack-plugin.js
+++ sqip-webpack-plugin.js
@@ -8,18 +8,16 @@ class SqipWebpackPlugin {
       (compilation) => {
         const [HtmlWebpackPlugin] = compiler.options.plugins.filter(
           (plugin) => plugin.constructor.name === 'HtmlWebpackPlugin');
-        // console.log('Lenght: ', compiler.options.plugins.filter(
-        //   (plugin) => plugin.constructor.name === 'HtmlWebpackPlugin').length)
         const hook = HtmlWebpackPlugin.constructor.getHooks(compilation).beforeEmit;
 
         hook.tapPromise(
           this.name,
           (htmlPluginData) => {
-            return new Promise(resolve => {
-              // console.log(htmlPluginData.html)
-              htmlPluginData.html = htmlPluginData.html.replace('/media/img/frontend/vs_sw_screenshotII.png', 'https://www.themasculinetraveler.com/wp-content/uploads/2018/02/Helsinki-Finland.jpg')
-                // = 'heh heh heh html5'
-              // return htmlPluginData
+            return new Promise((resolve, reject) => {
+              const oldImage = '/media/img/frontend/vs_sw_screenshotII.png'
+              const newImage = 'https://www.urlaubsguru.at/wp-content/uploads/2017/03/sommer-von-helsinki-finnland-istock_000024367312_large-2.jpg'
+              htmlPluginData.html = htmlPluginData.html.replace(oldImage, newImage)
+
               resolve(htmlPluginData)
             })
           }
