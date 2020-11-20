73e916997b987f53b0a1b94c7deab5c519db6d1e
rodionov80 <rodionov134@gmail.com>
2020-11-20 06:06:38 +0300

    Make sqip-webpack-plugin (+dev version)

--- sqip-webpack-plugin.js
+++ sqip-webpack-plugin.js
@@ -1,27 +1,39 @@
 const path = require('path')
 const sqip = require('sqip')
+const { encode } = require('js-base64')
 
 class SqipWebpackPlugin {
-  constructor() {
+  constructor () {
     this.name = 'SqipWebpackPlugin'
     this.htmlAttributeRegexp = /sqip-webpack-plugin-src=["'](.+?)\.(jpg|webp|jpeg|png|svg)['"]/gi
     this.imageUrlRegexp = /(?<=sqip-webpack-plugin-src=["'])(.+?)\.(jpg|webp|jpeg|png|svg)/gi
   }
 
-  getAttributes(html) {
+  getAttributes (html) {
     return html.match(this.htmlAttributeRegexp)
   }
 
-  getImagePath(attribute) {
+  getImagePath (attribute) {
     const [parsedPath] = attribute.match(this.imageUrlRegexp)
     return path.join(__dirname, 'src', parsedPath)
   }
 
-  createSrcAttribute(svg_base64encoded) {
+  createSrcAttribute (svg_base64encoded) {
     return `src='data:image/svg+xml;base64,${svg_base64encoded}'`
   }
 
-  apply(compiler) {
+  createDefaultBase64Placeholder() {
+    const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200" viewBox="0 0 300 200">
+      <rect fill="#eee" width="300" height="200"/>
+      <text fill="rgba(0,0,0,0.67)" font-family="sans-serif" font-size="30" dy="10.5" font-weight="normal" x="50%" y="50%" text-anchor="middle">
+      Placeholder
+</text>
+    </svg>`
+
+    return encode(svg)
+  }
+
+  apply (compiler) {
     compiler.hooks.compilation.tap(
       this.name,
       (compilation) => {
@@ -36,7 +48,15 @@ class SqipWebpackPlugin {
             const attributes = this.getAttributes(html)
             if (Array.isArray(attributes)) {
               const promises = await attributes.map(async attribute => {
+                const { mode } = compiler.options
+                if (mode !== 'production') {
+                  const placeholder = this.createDefaultBase64Placeholder()
+                  let converted = {}
+                  converted.svg_base64encoded = placeholder
+                  return converted
+                }
                 const imagePath = this.getImagePath(attribute)
+                console.log(`Making sqip placeholder:\n${imagePath}`)
                 return sqip({
                   filename: imagePath,
                 })
@@ -48,7 +68,6 @@ class SqipWebpackPlugin {
                 const { svg_base64encoded } = converted
                 const newAttribute = this.createSrcAttribute(svg_base64encoded)
                 html = html.replace(currentAttribute, newAttribute)
-                // console.log({converted})
               })
             }
 
