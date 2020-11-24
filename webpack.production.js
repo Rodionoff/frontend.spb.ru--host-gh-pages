5205f6d181e234ac4e6bd29fc323197d6802bbde
rodionov80 <rodionov134@gmail.com>
2020-11-24 08:41:58 +0300

    Restore copy-webpack-plugin import

--- webpack.production.js
+++ webpack.production.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
-// const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+// const MiniCssEx tractPlugin = require('mini-css-extract-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
